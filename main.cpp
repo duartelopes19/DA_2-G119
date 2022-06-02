3dcb79af3c7ce1fc36bf19e96b810021002c09fe
arcade <brunocastanheira1994@gmail.com>
2022-06-02 16:00:57 +0100

    Duration added

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <vector>
+#include "edmondskarp.cpp"
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n, e, a, b, c, d;
+
+    cin >> n;
+    Graph *g = new Graph(n);
+    cin >> e;
+    for (int i=0; i<e; i++) {
+        cin >> a >> b >> c >> d;
+        g->addLink(a, b, c, d);
+    }
+
+    // Execucao exemplo usando 1 como no orig   em a 4 como o destino
+    int flow = g->maxFlow_Capacity(1, 10);
+    cout << "Capacidade máxima de transporte do grafo 1 ao 10: " << flow << endl;
+    int flow_duration = g->maxFlow_Duration(1, 10);
+    cout << "Duração minima de transporte do grafo 1 ao 10:" << flow_duration << endl;
+
     return 0;
 }
